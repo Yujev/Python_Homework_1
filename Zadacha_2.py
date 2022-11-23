0369bd2f22e53ad076e5aa91a142a0a564764860
Yujev <ustatar@ya.ru>
2022-11-23 13:52:39 +0300

    Задача_2 таблица истинности с предикатами. Изменил решение

--- Zadacha_2.py
+++ Zadacha_2.py
@@ -1,11 +1,17 @@
 # Задача 2
 # Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
+# X Y Z     X Y Z
+# 1 1 1     0 0 0
+# 1 1 0     0 0 1
+# 1 0 1     0 1 0
+# 0 1 1     1 0 0
 
-x = bool(input("Введи значение X: "))
-y = bool(input("Введи значение Y: "))
-z = bool(input("Введи значение Z: "))
 
-if not(x or y or z) == (not x and not y and not z):
-    print(True)
-else:
-    print(False)
+for x in 0, 1:
+    for y in 0, 1:
+        for z in 0, 1:
+            if not (x or y or z) == (not x and not y and not z):
+                print(f' {x} {y} {z} - True')
+            else:
+                print(f' {x} {y} {z} - False')
+
